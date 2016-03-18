6e1baee6df7a2073908ddafad0e24bd5d342a9c4
Amitabh Kant <amitabhkant@gmail.com>
2016-03-18 14:46:10 +0530

    Corrected comments

--- autorium.ino
+++ autorium.ino
@@ -17,14 +17,21 @@ void setup() {
 
 void loop() {
   
-  // Test Code for relay
+  test_8_board_relay();
+
+
+}
+
+// Test Code for 8 port relay
+void test_8_board_relay(){
 
   for(int iCounter=31;iCounter < 39;iCounter++){
   
-    digitalWrite(iCounter, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500);              // wait for a second
-    digitalWrite(iCounter, LOW);    // turn the LED off by making the voltage LOW
-    delay(400);             // wait for a second
+    digitalWrite(iCounter, HIGH);   // turn the Relay on (HIGH is the voltage level)
+    delay(500);              // wait for 500 milliseconds
+    digitalWrite(iCounter, LOW);    // turn the relay off by making the voltage LOW
+    delay(400);             // wait for 400 millisecond
   }
-
+  
 }
+
