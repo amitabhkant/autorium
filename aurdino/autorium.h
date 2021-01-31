d9cbfe815949aaaf35295369d11cd8a37e0c67cb
Amitabh Kant <amitabhkant@gmail.com>
2021-01-31 18:40:31 +0530

    Data type changes
    Changed most pin numbers from int to byte

--- aurdino/autorium.h
+++ aurdino/autorium.h
@@ -1,81 +1,82 @@
 #define AUTORIUM_VERSION 3.0
-
-// 3.0 First working prototype of the aquarium automation. 
-
-
-// LED pins
-#define RED_LED_PIN 41
-#define YELLOW_LED_PIN 40
-#define GREEN_LED_PIN 39
-
-// I2C addresses
-#define LCD_I2C_ADDRESS 0x27
-#define RTC_I2C_ADDRESS 0x68
-#define EEPROM_I2C_ADDRESS 0x57
-
-#define TONE_PIN 8 // Pin speaker/piezo is connected (Include a 100 ohm resistor).
-
-//** Any definitions below this point might be changed into varaibles later, so they are following camel case notation **//
-
 #define ultrasonicMaxDistance 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+// Version 3.0 - First working prototype of the aquarium automation. 
+
 
 //** Variable defintions **//
 
-// *********************
-// Variable definitions for version 3.0 starts
-// Pin numbers are temporary. Need to set the right pin numbers
+// TODO: Pin numbers are temporary. Need to set the right pin numbers
 
 // Pin number for lights / 220 V AC / Solid State Relay
-int arLightRelayPin[] = {1, 2, 3};
+byte arLightRelayPin[] = {1, 2, 3};
 
 // Pin number for air pumps  / 220 V AC / Solid State Relay
-int arAirPumpRelayPin[] = {4, 5};
+byte arAirPumpRelayPin[] = {4, 5};
 
 
 //Pin number for heaters  / 220 V AC / Solid State Relay
-int arHeaterRelayPin[] = {6, 7, 8};
+byte arHeaterRelayPin[] = {6, 7, 8};
 
 // Pin number for solenoid valves (NO - Normally Open; NC - Normally Closed)  / 12 V DC / Mechanical Relay
-int arSolenoidRelayPin[] = {9, 10, 11, 12, 13, 14};
-// 0 - NC - From Aquarium to Filter // NC - From Aquarium to Drain // NC - From Filter to Aquarium // NC - From Filter to Aquarium // NC - From Filter to Drain // NC - Freshwater to Aquarium // NC - Freshwater to Filter
+byte arNCSolenoidRelayPin[] = {9, 10, 11, 12};
+
+// Pin assignments for the solenoid pins in the array should be fixed
+// arNCSolenoidRelayPin[0] -> NC - From Aquarium to Drain
+// arNCSolenoidRelayPin[1] -> NC - From Filter to Drain
+// arNCSolenoidRelayPin[2] -> NC - Freshwater to Aquarium
+// arNCSolenoidRelayPin[3] -> NC - Freshwater to Filter 
+
+byte arNOSolenoidRelayPin[] = {13, 14};
+// arNCSolenoidRelayPin[0] -> NO - From Aquarium to Filter
+// arNCSolenoidRelayPin[1] -> NO - From Filter to Aquarium 
+
 
 // Pin number for flow sensors. Must be interrupt pins. Array index follows solenoid valves array index
-int arFlowSensorPin[] = {15, 16, 17, 18, 19, 20};
+byte arFlowSensorPin[] = {15, 16, 17, 18, 19, 20};
 
 // Pin number for aquarium ultrasonic sensor
-int aqUltrasonicSensorTriggerPin = 20;
-int aqUltrasonicSensorEchoPin = 21;
+byte aqUltrasonicSensorTriggerPin = 20;
+byte aqUltrasonicSensorEchoPin = 21;
 
 // Pin number for filter float sensor
-int flFloatSensorPin = 22;
+byte flFloatSensorHighPin = 22;
+byte flFloatSensorLowPin = 22;
 
-// Pin number for water pump in aquarium / 220 V/ Solid State Relay
-int aqPumpRelayPin = 23;
+// Pin number for filter overflow float sensor
+byte flOverFlowFloatSensorHighPin = 22;
+byte flOverFlowFloatSensorLowPin = 22;
 
-// Pin number for water pump in filter / 220 V/ Solid State Relay
-int flPumpRelayPin = 24;
+// Pin number for water pump in aquarium/ 220 V/ Solid State Relay
+byte aqPumpRelayPin = 23;
+
+// Pin number for water pump in filter/ 220 V/ Solid State Relay
+byte flPumpRelayPin = 24;
+
+// Pin number for water pump in overflow section of filter/ 220 V/ Solid State Relay
+byte flOverFlowPumpRelayPin = 24;
 
 //Pin number for temperature sensor
-int arTemperatureSensorPin[] = {6,7,8};
+byte arTemperatureSensorPin[] = {6,7,8};
 
 
+// Aquarium dimensions in centimeters
+// Calculations are based on this page: https://www.calculatorsoup.com/calculators/construction/tank.php
+int aqHeight = 91; 
+int aqLength = 183; 
+int aqWidth = 61;
+int aqFilledDepth = 85;
+byte aqShape = 0;           // 0 - Cuboid/Cube; 1 - Cylindrical. Can be used to calculate water volume
+
+byte autoriumState = 0;     // Stores the current action being performed in the Autorium:
+                            // 0 -> No action
+                            // 1 -> Standard filter operation
+                            // 2 -> Aquarium water drain
+                            // 3 -> Aquarium fresh water filling
+                            // 4 -> Filter water drain
+                            // 5 -> Filter fresh water filling
 
 
-// Variable definitions for version 3.0 ends
-// *********************
-
-
-
-
-// Define your aquarium dimensions in centimeters
-int aquariumHeight = 63; //70      // Z Axis
-int aquariumLength = 135; //130      // Y Axis
-int aquariumWidth = 43;  //45        // X Axis
-int aquariumShape = 0;         // 0 - Rectangle/Cube; 1 - Circular/Cylindrical. Can be used to calculate water volume
-
-// Define water level in centimeters from bottom
-int maxWaterLevel = 54;
-int minWaterLevel = 10;
 
 
 int arWaterLevel[10];       // Store last 10 valaue from the water level. Discard out of range values
@@ -83,7 +84,7 @@ int arWaterLevel[10];       // Store last 10 valaue from the water level. Discar
 
 byte autoriumCurrentState = 0;                 // Current action being performed by Autorium: 0 - None; 1 - Water Extract; 2 - Water Refill; 3 - Both
 byte autoriumOperationMode = 0;                // Operation mode for Autrium: 0 - Simple extraction and refill;1 - Time based; 2 - Based on sensors input (Not implemented); 3 - Combination of 1 & 2 
-byte autoriumState = 0;                        // This value is being stored in EEPROM. Values same as autoriumCurrentState
+//byte autoriumState = 0;                        // This value is being stored in EEPROM. Values same as autoriumCurrentState
 
 
 
@@ -107,15 +108,31 @@ volatile long outwardFlowCount = 0L;            // Count from the outward flow s
 //** Function Definitions
 
 
-// *********************
-// Function definitions for version 3.0 starts
+// Read all the temperature sensors
 
-// Read all the temperatrure sensors
-
-// Initialize all flow sensor pins as interrupt pins
+// Initialize all flow sensor pins as interrupt pins. This is to be done individually for every flow sensor pin as this would require individual call back functions. Best to move it to setup directly
 // attachInterrupt(digitalPinToInterrupt(2), pin_ISR, CHANGE);
 
-// Function definitions for version 3.0 ends
+
+// All soelnoid valve to their normal state (wihtout power being passed on )
+// If closeNoValves is set to 1, Normally open valves will also be powered on (will be closed)
+void resetAllPins(int closeNOValves = 0);
+
+// Drain water from the aquarium. Close all solenoid valves apart from Aquarium->Drain one
+void drainAquarium();
+
+// Drain water from the filter. Close all solenoid valves apart from Aquarium->Drain one
+void drainFilter();
+
+// Fill fresh water in the aquarium
+void fillAquarium();
+
+// Fill fresh waster in the filter
+void fillFilter();
+
+// Recycle the aquarium water through the filter. This is the main routine that needs to be running most of the time
+void operateFilter();
+
 // *********************
 
 
--- aurdino/autorium.ino
+++ aurdino/autorium.ino
@@ -8,6 +8,8 @@
    *      Connect Water Extractor Pump, Temperature Sensor, Heater, Light, Air Pump & Filter Pump to Relay
    *      Integrate ESP32 chip for wifi connectivity
    *      Relay test does no longer need to switch on and off all the ports
+   *      Variables strating with 'aq' are connected to Main Aquarium, those starting with 'fl' are connected to the Filter/Sump
+   * 
    *      
    *      Steps:
    *      a) Complete the aquarium filter combination first
@@ -18,73 +20,76 @@ void setup(void) {
 
   Serial.println("** Start of Setup **");
   
-  int arrayCount;   // variable to store array count
+  byte arrayCount;   // variable to store array count
 
-  /*
   // initialize light relay pins
   arrayCount = sizeof(arLightRelayPin) / sizeof(arLightRelayPin[0]);
-  for (int thisPin = 0; thisPin < arrayCount; thisPin++) {
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
     pinMode(arLightRelayPin[thisPin], OUTPUT);
-    digitalWrite(arLightRelayPin[thisPin], LOW); // switch off all the lights
   }
-  */
 
-  /*
   // initialize air pump relay pins
   arrayCount = sizeof(arAirPumpRelayPin) / sizeof(arAirPumpRelayPin[0]);
-  for (int thisPin = 0; thisPin < arrayCount; thisPin++) {
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
     pinMode(arAirPumpRelayPin[thisPin], OUTPUT);
-    digitalWrite(arAirPumpRelayPin[thisPin], LOW); // switch off air pumps
   }
-  */
 
-  /*
   // initialize heater relay pins
   arrayCount = sizeof(arHeaterRelayPin) / sizeof(arHeaterRelayPin[0]);
-  for (int thisPin = 0; thisPin < arrayCount; thisPin++) {
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
     pinMode(arHeaterRelayPin[thisPin], OUTPUT);
-    digitalWrite(arHeaterRelayPin[thisPin], LOW); // switch off heaters
   }
-  */
-
 
   // initialize solenoid relay pins
-  arrayCount = sizeof(arSolenoidRelayPin) / sizeof(arSolenoidRelayPin[0]);
-  for (int thisPin = 0; thisPin < arrayCount; thisPin++) {
-    pinMode(arSolenoidRelayPin[thisPin], OUTPUT);
-    digitalWrite(arSolenoidRelayPin[thisPin], LOW); // switch off solenoid valves
+  arrayCount = sizeof(arNOSolenoidRelayPin) / sizeof(arNOSolenoidRelayPin[0]);
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+    pinMode(arNOSolenoidRelayPin[thisPin], OUTPUT);
   }
 
+  arrayCount = sizeof(arNCSolenoidRelayPin) / sizeof(arNCSolenoidRelayPin[0]);
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+    pinMode(arNCSolenoidRelayPin[thisPin], OUTPUT);
+  }
+  
   // initialize flow sensor pins
   arrayCount = sizeof(arFlowSensorPin) / sizeof(arFlowSensorPin[0]);
-  for (int thisPin = 0; thisPin < arrayCount; thisPin++) {
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
     pinMode(arFlowSensorPin[thisPin], INPUT);
     // attachInterrupt requires callback function, which should be unique to each interrupt
     //attachInterrupt(digitalPinToInterrupt(arFlowSensorPin[thisPin], inwardFlowSensor, RISING);
   }
   
-  // switch off both the motor pumps
+  // initialize motor relay pumps
   pinMode(aqPumpRelayPin, OUTPUT);
-  digitalWrite(aqPumpRelayPin, LOW); // switch off aquarium pump
   pinMode(aqPumpRelayPin, OUTPUT);
-  digitalWrite(flPumpRelayPin, LOW); // switch off filter pump
 
   // initialize aquarium ultrasnoic sensor pins
   pinMode(aqUltrasonicSensorTriggerPin, OUTPUT);
   pinMode(aqUltrasonicSensorEchoPin, INPUT);
 
-  // initialize filter float sensor pin
-  pinMode(flFloatSensorPin, INPUT_PULLUP);
+  // initialize filter float sensor pins
+  pinMode(flFloatSensorHighPin, INPUT_PULLUP);
+  pinMode(flFloatSensorLowPin, INPUT_PULLUP);
+  pinMode(flOverFlowFloatSensorHighPin, INPUT_PULLUP);
+  pinMode(flOverFlowFloatSensorLowPin, INPUT_PULLUP);
 
+  // set all the pins to safe values
+  resetAllPins();
 
+  // TODO: Initialize the ESP32 board RS232 ports
   
-  //initRelayBoard(31, 8);      // Led blink test on all active ports
-
-
   Serial.begin(9600);  // Initialise the serial port. This would be the same serial port used for programming the board.
   Serial.print("Starting Autorium version ");
   Serial.println(AUTORIUM_VERSION);
   Serial.println("Debug Mode is ON");
+  Serial.print("Aquarium Height -> ");
+  Serial.println(aqHeight);
+  Serial.print("Aquarium Width -> ");
+  Serial.println(aqWidth);
+  Serial.print("Aquarium Length -> ");
+  Serial.println(aqLength);
+  Serial.print("Aquarium Filled Depth -> ");
+  Serial.println(aqFilledDepth);
   
   //i2cScanner();         // Run the I2C scanner to check for peripherals
 
@@ -100,6 +105,161 @@ void loop(void) {
   
     
   Serial.println("** End of Loop **");
+}
+
+void operateFilter(){
+  if(autoriumState==1){
+    // TODO: Filtering is in process, keep checking for water level in the aquarium, filter and overflow section. Also need to check for the temperature
+    // TODO: At a later stage, we need to inetgerate the DO/PH etc sensors
+    // TODO: Operation of motors (aquarium and filter should be done here. Water level needs to be checked before operating the motor) 
+  }else{
+
+    // Start the filtering process
+
+    // Reset all to safe values
+    resetAllPins(1);
+
+    // Drain filter first
+    drainFilter();
+
+    // Open up the solenoid valve from aquarium to filter and vice versa. These are NO valves, so we need to set the pin to LOW
+    digitalWrite(arNOSolenoidRelayPin[0], LOW);   //From Aquarium to Filter
+    digitalWrite(arNOSolenoidRelayPin[1], LOW);   //From Filter to Aquarium
+
+
+  }
+}
+
+void fillFilter(){
+
+  if(autoriumState==5){
+    // TODO: Filling of filter is in process, keep checking for water level in the filter and overflow section
+  }else{
+    // Start the filling process for the filter 
+
+    // Reset all to safe values
+    resetAllPins(1);
+
+    // Open up the solenoid valve for freshwater input to filter 
+    digitalWrite(arNCSolenoidRelayPin[3], HIGH);
+
+    // set the autorium state
+    autoriumState = 5;
+  }
+}
+
+void fillAquarium(){
+
+  if(autoriumState==3){
+    // TODO: Filling of Aquarium is in process, keep checking for water level in the aquarium
+  }else{
+    // Start the filling process for the aquarium 
+
+    // Reset all to safe values
+    resetAllPins(1);
+
+    // Open up the solenoid valve for freshwater input to aquarium 
+    digitalWrite(arNCSolenoidRelayPin[2], HIGH);
+
+    // set the autorium state
+    autoriumState = 3;
+  }
+}
+
+void drainAquarium(){
+  
+  if(autoriumState==2){
+    // TODO: Draining of Aquarium is in process, keep checking for water level in the aquarium
+  }else{
+    // Start the draining process for the aquarium
+
+    // Reset all to safe values
+    resetAllPins(1);
+
+    // Open up the solenoid valve which leads from the aquarium to drain
+    digitalWrite(arNCSolenoidRelayPin[0], HIGH);
+
+    // Start the aquarium motor
+    digitalWrite(aqPumpRelayPin, HIGH);
+
+    // set the autorium state
+    autoriumState = 2;
+  }
+}
+
+void drainFilter(){
+  
+  if(autoriumState==4){
+    // TODO: Draining of Filter is in process, keep checking for water level in the filter and the overflow section
+  }else{
+    // Start the draining process for the filter
+
+    // Reset all to safe values
+    resetAllPins(1);
+
+    // Open up the solenoid valve which leads from the filter to drain
+    digitalWrite(arNCSolenoidRelayPin[1], HIGH);
+
+    // Start the filter motor
+    digitalWrite(flPumpRelayPin, HIGH);
+
+    // Start the overflow motor
+    digitalWrite(flOverFlowPumpRelayPin, HIGH);
+
+    // set the autorium state
+    autoriumState = 4;
+  }
+
+}
+
+void resetAllPins(int closeNOValves = 0){
+  
+  byte arrayCount;   // variable to store array count
+  
+  // switch off all the lights
+  arrayCount = sizeof(arLightRelayPin) / sizeof(arLightRelayPin[0]);
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+    digitalWrite(arLightRelayPin[thisPin], LOW); 
+  }
+
+  // switch off air pumps
+  arrayCount = sizeof(arAirPumpRelayPin) / sizeof(arAirPumpRelayPin[0]);
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+    digitalWrite(arAirPumpRelayPin[thisPin], LOW); 
+  }
+
+  // switch off heaters
+  arrayCount = sizeof(arHeaterRelayPin) / sizeof(arHeaterRelayPin[0]);
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+    digitalWrite(arHeaterRelayPin[thisPin], LOW); 
+  }
+
+  // opens solenoid valves, =these are normally open ones
+  if(closeNOValves == 1){
+    arrayCount = sizeof(arNOSolenoidRelayPin) / sizeof(arNOSolenoidRelayPin[0]);
+    for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+      digitalWrite(arNOSolenoidRelayPin[thisPin], HIGH); 
+    }
+  }else{
+    arrayCount = sizeof(arNOSolenoidRelayPin) / sizeof(arNOSolenoidRelayPin[0]);
+    for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+      digitalWrite(arNOSolenoidRelayPin[thisPin], LOW); 
+    }
+  }
+
+  // close solenoid valves,  these are normally closed ones
+  arrayCount = sizeof(arNCSolenoidRelayPin) / sizeof(arNCSolenoidRelayPin[0]);
+  for (byte thisPin = 0; thisPin < arrayCount; thisPin++) {
+    digitalWrite(arNCSolenoidRelayPin[thisPin], LOW); 
+  }
+
+  // switch off both the motor pumps
+  digitalWrite(aqPumpRelayPin, LOW); // switch off aquarium pump
+  digitalWrite(flPumpRelayPin, LOW); // switch off filter pump
+
+  // set the autorium state
+  autoriumState = 0;
+
 }
 
 /*
